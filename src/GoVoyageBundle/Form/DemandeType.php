2ca915b3c39033e21ea31195ca1af9d28c683340
Asus <wajdy.bouslama@esprit.tn>
2017-11-10 00:45:34 +0100

    new commit2

---
+++ src/GoVoyageBundle/Form/DemandeType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace GoVoyageBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DemandeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('idGuideFk')->add('idClientFk')->add('idVpFk');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'GoVoyageBundle\Entity\Demande'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'govoyagebundle_demande';
+    }
+
+
+}
--- src/GoVoyageBundle/Form/VoyagepersonaliseType.php
+++ src/GoVoyageBundle/Form/VoyagepersonaliseType.php
@@ -5,6 +5,7 @@ namespace GoVoyageBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class VoyagepersonaliseType extends AbstractType
 {
@@ -13,7 +14,16 @@ class VoyagepersonaliseType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nom')->add('villeDepart')->add('villeArrive')->add('dateDepart')->add('dateArrive')->add('nbrParticipant')->add('hotelFk')->add('clientVpFk')->add('event1Fk')->add('idGuideFk');
+        $builder->add('nom')
+            ->add('villeDepart')
+            ->add('villeArrive')
+            ->add('dateDepart')
+            ->add('dateArrive')
+            ->add('nbrParticipant')
+            ->add('hotelFk',EntityType::class,array('class'=>'GoVoyageBundle:Users','choice_label'=>'idUser','multiple'=>false))
+            ->add('clientVpFk',EntityType::class,array('class'=>'GoVoyageBundle:Users','choice_label'=>'idUser','multiple'=>false))
+            ->add('event1Fk',EntityType::class,array('class'=>'GoVoyageBundle:Evenement','choice_label'=>'idEvenement','multiple'=>false))
+            ->add('idGuideFk',EntityType::class,array('class'=>'GoVoyageBundle:Users','choice_label'=>'idUser','multiple'=>false));
     }
     
     /**
